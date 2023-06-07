a07cf81ba2c525d8f5443083355df286fa5754af
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-06-07 16:04:01 +0300

    navigation to bottom

--- features/bottom/src/main/java/com/dashkevich/bottom/BottomFragment.kt
+++ features/bottom/src/main/java/com/dashkevich/bottom/BottomFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 class BottomFragment : Fragment(R.layout.fragment_bottom) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Toast.makeText(requireContext(), "Bottom frag!", Toast.LENGTH_SHORT).show()
     }
 }
--- navigation/src/main/java/com/dashkevich/navigation/BlankFragment.kt
+++ navigation/src/main/java/com/dashkevich/navigation/BlankFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 class BlankFragment : Fragment(R.layout.fragment_blank) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        findNavController().navigate(R.id.action_blankFragment_to_bottom)
     }
 
 }
