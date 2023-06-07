f30cd1e0d775f74c1504079ba91d4c1588070c93
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-06-07 15:51:57 +0300

    bottom screen created - The screen containing the lower toolbar has been created.

---
+++ features/bottom/src/main/java/com/dashkevich/bottom/BottomFragment.kt
@@ -0,0 +1,14 @@
+package com.dashkevich.bottom
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class BottomFragment : Fragment(R.layout.fragment_bottom) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
