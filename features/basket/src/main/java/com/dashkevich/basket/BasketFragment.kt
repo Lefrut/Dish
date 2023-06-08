2155ff62696a2c4cebce2f11554bb2b6188f78cd
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-06-08 13:44:34 +0300

    create bottom navigation - The bottom navigation graph and accompanying screen graphs have been created. The navigation logic is attached to the bottom_bar using the setUpWithNavController() function. The necessary screens have been created, the rest are empty.

--- features/basket/src/main/java/com/dashkevich/basket/BasketFragment.kt
+++ features/basket/src/main/java/com/dashkevich/basket/BasketFragment.kt
@@ -1,14 +1,11 @@
-package com.dashkevich.navigation
+package com.dashkevich.basket
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 
-class BlankFragment : Fragment(R.layout.fragment_blank) {
 
+class BasketFragment : Fragment(R.layout.fragment_basket) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- features/home/src/main/java/com/dashkevich/home/HomeFragment.kt
+++ features/home/src/main/java/com/dashkevich/home/HomeFragment.kt
@@ -1,14 +1,12 @@
-package com.dashkevich.navigation
+package com.dashkevich.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 
-class BlankFragment : Fragment(R.layout.fragment_blank) {
-
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
