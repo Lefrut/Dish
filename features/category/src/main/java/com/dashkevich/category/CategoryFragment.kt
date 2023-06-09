1f7d2ab377e58f610a9525eaab8db783a02313ad
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-06-09 13:43:38 +0300

    create category screen

---
+++ features/category/src/main/java/com/dashkevich/category/CategoryFragment.kt
@@ -0,0 +1,19 @@
+package com.dashkevich.category
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dashkevich.category.databinding.FragmentCategoryBinding
+
+
+class CategoryFragment : Fragment(R.layout.fragment_category) {
+
+    private lateinit var binding: FragmentCategoryBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCategoryBinding.bind(view)
+    }
+}
