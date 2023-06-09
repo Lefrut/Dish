995f43cbdb8754895c1fba42e565e3a57baba183
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-06-09 14:06:18 +0300

    create category top bar

--- features/category/src/main/java/com/dashkevich/category/CategoryFragment.kt
+++ features/category/src/main/java/com/dashkevich/category/CategoryFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.dashkevich.category.databinding.FragmentCategoryBinding
 
 
@@ -15,5 +16,8 @@ class CategoryFragment : Fragment(R.layout.fragment_category) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentCategoryBinding.bind(view)
+        binding.backArrow.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 }
