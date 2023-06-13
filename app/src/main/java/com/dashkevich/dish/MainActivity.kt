8a1cd039a22405705a3f0560589b4ab08a83935a
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-06-13 16:06:44 +0300

    status bar config

--- app/src/main/java/com/dashkevich/dish/MainActivity.kt
+++ app/src/main/java/com/dashkevich/dish/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.dashkevich.dish
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 
@@ -16,5 +18,21 @@ class MainActivity : AppCompatActivity() {
         navController = (supportFragmentManager.findFragmentById(R.id.root_container) as NavHostFragment)
                 .navController
         navController.setGraph(com.dashkevich.navigation.R.navigation.root_nav_graph)
+        configSystemUI()
+    }
+
+    private fun configSystemUI() {
+        val wic = WindowInsetsControllerCompat(window, window.decorView)
+
+        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_NO -> {
+                wic.isAppearanceLightStatusBars = true
+                wic.isAppearanceLightNavigationBars = true
+            }
+            Configuration.UI_MODE_NIGHT_YES -> {
+                wic.isAppearanceLightStatusBars = true
+                wic.isAppearanceLightNavigationBars = true
+            }
+        }
     }
 }
